4a2b320424be6db6312717167028ccdecabf1f6d
Narender Singh <narender2031@gmail.com>
2019-06-16 15:24:13 +0530

    Fix confirmation Email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protect_from_forgery with: :exception, prepend: true
+  
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone])
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 # home controller
 
 class HomeController < ApplicationController
-  before_action :authenticate_user!
   def index
   end
 end
